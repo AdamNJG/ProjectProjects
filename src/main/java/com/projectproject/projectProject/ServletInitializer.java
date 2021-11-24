1b5dfd4d0d8f6c2e74d6541853a7fc49af35be17
Adam <Adam.goldsmith0@gmail.com>
2021-11-24 09:51:16 +0000

    Added Spring Boot

---
+++ src/main/java/com/projectproject/projectProject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.projectproject.projectProject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectProjectApplication.class);
+	}
+
+}
---
+++ src/test/java/com/projectproject/projectProject/ProjectProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectproject.projectProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
